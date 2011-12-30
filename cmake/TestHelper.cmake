04c105f716b928cb647890e6668cfae1c0ec5209
Travis Abbott <tabbott@genome.wustl.edu>
2011-12-30 16:09:14 -0600

    new add_integration_test cmake macro

--- cmake/TestHelper.cmake
+++ cmake/TestHelper.cmake
@@ -9,3 +9,11 @@ macro(def_test testName)
 
     set_tests_properties(Test${testName} PROPERTIES LABELS unit)
 endmacro(def_test testName)
+
+macro(def_integration_test exe_tgt testName script)
+    add_test(
+        NAME ${testName}
+        COMMAND sh -ec "PYTHONPATH='${CMAKE_SOURCE_DIR}/build-common/python:$ENV{PYTHONPATH}' ${CMAKE_CURRENT_SOURCE_DIR}/${script} $<TARGET_FILE:${exe_tgt}>"
+    )
+    set_tests_properties(${testName} PROPERTIES LABELS integration)
+endmacro(def_integration_test testName script)
