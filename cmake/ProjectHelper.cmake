f4344fef471324b23ccec3c2fe94b0cfcea0b36b
Travis Abbott <tabbott@genome.wustl.edu>
2013-05-06 15:14:37 -0500

    add coverage build type

--- cmake/ProjectHelper.cmake
+++ cmake/ProjectHelper.cmake
@@ -1,9 +1,16 @@
 set(CMAKE_CXX_FLAGS_PACKAGE ${CMAKE_CXX_FLAGS_RELEASE})
 set(CMAKE_C_FLAGS_PACKAGE ${CMAKE_CXX_FLAGS_RELEASE})
 
+if (CMAKE_COMPILER_IS_GNUCC AND CMAKE_COMPILER_IS_GNUCXX)
+    SET(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG} --coverage")
+    SET(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG} --coverage")
+    SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG} --coverage")
+endif ()
+
+
 if (NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE release CACHE STRING
-        "Options: None Debug Release Package RelWithDebInfo MinSizeRel." FORCE)
+        "Options: None Debug Release Package RelWithDebInfo MinSizeRel Coverage." FORCE)
     message("No CMAKE_BUILD_TYPE specified, defaulting to release")
 endif ()
 
