28f2df445cddd7c09c09d79d3d30bddd458da223
Travis Abbott <tabbott@genome.wustl.edu>
2013-07-25 16:08:21 -0500

    add ability to prepend to pythonpath for integration tests

--- cmake/TestHelper.cmake
+++ cmake/TestHelper.cmake
@@ -61,9 +61,10 @@ macro(def_test testName)
 endmacro(def_test testName)
 
 macro(def_integration_test exe_tgt testName script)
+    set(PPATH "${BC_PYTHONPATH_EXTRA}:${CMAKE_SOURCE_DIR}/build-common/python:$ENV{PYTHONPATH}")
     add_test(
         NAME ${testName}
-        COMMAND sh -ec "PYTHONPATH='${CMAKE_SOURCE_DIR}/build-common/python:$ENV{PYTHONPATH}' ${CMAKE_CURRENT_SOURCE_DIR}/${script} $<TARGET_FILE:${exe_tgt}>"
+        COMMAND sh -ec "PYTHONPATH='${PPATH}' ${CMAKE_CURRENT_SOURCE_DIR}/${script} $<TARGET_FILE:${exe_tgt}>"
     )
     set_tests_properties(${testName} PROPERTIES LABELS integration)
 endmacro(def_integration_test testName script)
