9d166cff21d10d233640d0d23e98b6c815306514
Dave Larson <dlarson@genome.wustl.edu>
2014-02-22 22:31:51 -0600

    incorporate fix from sjackman to prevent segfaults

--- src/lib/bamrc/BasicStat.cpp
+++ src/lib/bamrc/BasicStat.cpp
@@ -45,8 +45,9 @@ void BasicStat::process_read(bam_pileup1_t const* base) {
     //hopefully grab out our calculated per/read values
     //FIXME these will be unavailable if there is no reference
     //TODO Make sure the defaults on the above are reasonable if there is nothing available
-    uint8_t *tag_ptr = bam_aux_get(base->b, "Zm") + 1;
+    uint8_t *tag_ptr = bam_aux_get(base->b, "Zm");
     if(tag_ptr) {
+        ++tag_ptr;
         aux_zm_t zm = aux_zm_t::from_string((char const*)tag_ptr);
         mismatch_sum = zm.sum_of_mismatch_qualities;
         clipped_length = zm.clipped_length;
