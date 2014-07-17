a3eb727e791af33a281ded835f74c26154e54ce6
Travis Abbott <tabbott@genome.wustl.edu>
2014-07-17 17:35:09 -0500

    Squashed 'build-common/' changes from 667edee..d356b1b
    d356b1b always use gtests tr1/tuple
    8e9db39 make sure tests link w/threads lib
    
    git-subtree-dir: build-common
    git-subtree-split: d356b1b2f75c8078387e3e941af6c39154277cc0

--- cmake/TestHelper.cmake
+++ cmake/TestHelper.cmake
@@ -12,6 +12,7 @@ ExternalProject_Add(
     gtest160
     URL ${CMAKE_CURRENT_SOURCE_DIR}/build-common/vendor/gtest-1.6.0.tar.gz
     INSTALL_COMMAND ""
+    CMAKE_ARGS -DCMAKE_CXX_FLAGS=-DGTEST_USE_OWN_TR1_TUPLE=1
     BINARY_DIR ${GTEST_LIB_DIR}
     )
 ExternalProject_Get_Property(gtest160 source_dir)
@@ -33,7 +34,7 @@ set_property(TARGET gtest_main PROPERTY IMPORTED_LOCATION ${GTEST_MAIN_LIBRARY})
 macro(add_unit_tests test_name)
     set(src_files ${ARGN})
     add_executable(${test_name} ${src_files} ${COMMON_SOURCES})
-    target_link_libraries(${test_name} ${TEST_LIBS} gtest gtest_main)
+    target_link_libraries(${test_name} ${TEST_LIBS} gtest gtest_main ${CMAKE_THREAD_LIBS_INIT})
     add_dependencies(${test_name} gtest160)
     if($ENV{BC_UNIT_TEST_VG})
         add_test(
@@ -49,7 +50,7 @@ endmacro(add_unit_tests test_name src_files)
 
 macro(def_test testName)
     add_executable(Test${testName} Test${testName}.cpp ${COMMON_SOURCES})
-    target_link_libraries(Test${testName} ${TEST_LIBS} gtest gtest_main)
+    target_link_libraries(Test${testName} ${TEST_LIBS} gtest gtest_main ${CMAKE_THREAD_LIBS_INIT})
     add_dependencies(Test${testName} gtest160)
     if($ENV{BC_UNIT_TEST_VG})
         add_test(NAME Test${testName} COMMAND valgrind --leak-check=full --error-exitcode=1 $<TARGET_FILE:Test${testName}>)
