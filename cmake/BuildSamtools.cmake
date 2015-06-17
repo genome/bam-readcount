a719da3d3d5029e2d8eaa91c8da19e6d9331fa92
Travis Abbott <typedef.struct@gmail.com>
2015-06-16 20:03:55 -0500

    use new ExternalDependency_Add feature of build-common
    This sets up all of the proper dependencies on the 3rd party libs we
    build, removing the need to "make deps" before starting a parallel
    build.

--- cmake/BuildSamtools.cmake
+++ cmake/BuildSamtools.cmake
@@ -1,7 +1,5 @@
 cmake_minimum_required(VERSION 2.8)
 
-include(ExternalProject)
-
 set(SAMTOOLS_ROOT ${CMAKE_BINARY_DIR}/vendor/samtools)
 set(SAMTOOLS_LOG ${SAMTOOLS_ROOT}/build.log)
 set(SAMTOOLS_LIB ${SAMTOOLS_ROOT}/${CMAKE_FIND_LIBRARY_PREFIXES}bam${CMAKE_STATIC_LIBRARY_SUFFIX})
@@ -11,37 +9,36 @@ find_package(ZLIB)
 if (NOT ZLIB_FOUND)
     set(ZLIB_ROOT ${CMAKE_BINARY_DIR}/vendor/zlib)
     set(ZLIB_SRC ${CMAKE_BINARY_DIR}/vendor/zlib-src)
-    ExternalProject_Add(
-        zlib
-        URL ${CMAKE_SOURCE_DIR}/vendor/zlib-1.2.8.tar.gz
-        SOURCE_DIR ${ZLIB_SRC}
-        BINARY_DIR ${ZLIB_SRC}
-        CONFIGURE_COMMAND ./configure --prefix=${ZLIB_ROOT}
-        BUILD_COMMAND make
-        INSTALL_COMMAND make install
-    )
     set(ZLIB_INCLUDE_DIRS ${ZLIB_ROOT}/include)
     set(ZLIB_LIBRARIES ${ZLIB_ROOT}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}z${CMAKE_STATIC_LIBRARY_SUFFIX})
-    add_library(z STATIC IMPORTED)
-    set_property(TARGET z PROPERTY IMPORTED_LOCATION ${ZLIB_LIBRARIES})
+    ExternalDependency_Add(
+        zlib
+        BUILD_BYPRODUCTS ${ZLIB_LIBRARIES}
+        ARGS
+            URL ${CMAKE_SOURCE_DIR}/vendor/zlib-1.2.8.tar.gz
+            SOURCE_DIR ${ZLIB_SRC}
+            BINARY_DIR ${ZLIB_SRC}
+            CONFIGURE_COMMAND ./configure --prefix=${ZLIB_ROOT}
+            BUILD_COMMAND make
+            INSTALL_COMMAND make install
+    )
 endif (NOT ZLIB_FOUND)
 
-ExternalProject_Add(
+ExternalDependency_Add(
     samtools-lib
-    URL ${CMAKE_SOURCE_DIR}/vendor/samtools-0.1.19.tar.gz
-    SOURCE_DIR ${SAMTOOLS_ROOT}
-    BINARY_DIR ${SAMTOOLS_ROOT}
-    PATCH_COMMAND patch -p2 -t -N < ${CMAKE_SOURCE_DIR}/vendor/samtools0.1.19.patch
-    CONFIGURE_COMMAND echo "Building samtools, build log at ${SAMTOOLS_LOG}"
-    BUILD_COMMAND make INCLUDES=-I${ZLIB_INCLUDE_DIRS} libbam.a > ${SAMTOOLS_LOG} 2>&1
-    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS ${SAMTOOLS_LIB} ${SAMTOOLS_BIN}
+    ARGS
+        URL ${CMAKE_SOURCE_DIR}/vendor/samtools-0.1.19.tar.gz
+        SOURCE_DIR ${SAMTOOLS_ROOT}
+        BINARY_DIR ${SAMTOOLS_ROOT}
+        PATCH_COMMAND patch -p2 -t -N < ${CMAKE_SOURCE_DIR}/vendor/samtools0.1.19.patch
+        CONFIGURE_COMMAND echo "Building samtools, build log at ${SAMTOOLS_LOG}"
+        BUILD_COMMAND make INCLUDES=-I${ZLIB_INCLUDE_DIRS} libbam.a > ${SAMTOOLS_LOG} 2>&1
+        INSTALL_COMMAND "true"
 )
 
-add_library(bam STATIC IMPORTED)
-set_property(TARGET bam PROPERTY IMPORTED_LOCATION ${SAMTOOLS_LIB})
-
 set(Samtools_INCLUDE_DIRS ${ZLIB_INCLUDE_DIRS};${SAMTOOLS_ROOT})
-set(Samtools_LIBRARIES bam m ${ZLIB_LIBRARIES})
+set(Samtools_LIBRARIES ${SAMTOOLS_LIB} m ${ZLIB_LIBRARIES})
 
 if (NOT ZLIB_FOUND)
     add_dependencies(samtools-lib zlib)
