9a2ab34c222fd0ded7baa53651e3556b8a711fcf
Dave Larson <dlarson@genome.wustl.edu>
2015-12-19 23:18:54 -0600

    flesh out tests for IndelQueue

--- test/lib/bamrc/TestIndelQueue.cpp
+++ test/lib/bamrc/TestIndelQueue.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 
+#include <sstream>
 #include <memory>
 
 std::auto_ptr<ReadWarnings> WARN;
@@ -17,17 +18,74 @@ TEST(IndelQueue, push) {
     test_queue.push(entry);
     ASSERT_EQ(1, test_queue.queue.size());
 }
-/*
+
     
 TEST(IndelQueue, process_irrelevant) {
+    //This test verifies the queue is cleared of sites that are passed by
+    IndelQueue test_queue;
 
-}
+    IndelQueueEntry test1;
+    test1.tid = 1;
+    test1.pos = 1;
 
-TEST(IndelQueue, process_relevant) {
+    test_queue.push(test1);
 
+    IndelQueueEntry test2;
+    test2.tid = 1;
+    test2.pos = 5;
+
+    test_queue.push(test2);
+
+    stringstream stream;
+
+    test_queue.process(1, 2, stream);
+    ASSERT_EQ(1, test_queue.queue.size());
 }
 
 TEST(IndelQueue, process_new_chromosome) {
+    //This test verifies the queue is cleared of sites when the chromosome changes
+    IndelQueue test_queue;
+
+    IndelQueueEntry test1;
+    test1.tid = 1;
+    test1.pos = 1;
+
+    test_queue.push(test1);
+
+    IndelQueueEntry test2;
+    test2.tid = 1;
+    test2.pos = 5;
+
+    test_queue.push(test2);
+
+    stringstream stream;
+
+    test_queue.process(10, 2, stream);
+    ASSERT_EQ(0, test_queue.queue.size());
+}
+
+TEST(IndelQueue, process_relevant) {
+    IndelQueue test_queue;
+
+    IndelQueueEntry test1;
+    test1.tid = 1;
+    test1.pos = 1;
+
+    test_queue.push(test1);
+
+    IndelQueueEntry test2;
+    test2.tid = 1;
+    test2.pos = 5;
+    test2.indel_stats.read_count = 10;
+
+    test_queue.push(test2);
+
+    stringstream stream;
+    ASSERT_EQ(0, stream.str().size());
+
+    int depth = test_queue.process(1, 5, stream);
+    ASSERT_EQ(0, test_queue.queue.size());
+    ASSERT_EQ(10, depth);
+    ASSERT_NE(0, stream.str().size());
 
 }
-*/
